623e0d8a3403276ead223b720d85302bdfc342c3
deegane <eamonn.deegan@gmail.com>
2017-04-17 23:22:39 +0100

    update test case

--- test/model/MovieSpec.scala
+++ test/model/MovieSpec.scala
@@ -6,7 +6,7 @@ import org.scalatestplus.play._
 class MovieSpec extends PlaySpec {
 
   "A Movie" must {
-    "have a title" in {
+    "have a title and rating must match" in {
       val movie = new Movie("lethal weapon")
       val rating = new Rating("Internet Movie Database", "8.9")
       movie.ratings.add(rating)
